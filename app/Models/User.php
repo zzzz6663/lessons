f7bacd70264a5dc3d97b1448589025622044eec1
naser <na3r.jafari@gmail.com>
2024-04-27 06:55:29 +0330

    or0708

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,7 +93,7 @@ class User extends Authenticatable
         return $ca ;
     }
     public function balance(){
-        //
+        //ss
        return $this->transactions->whereStatus("payed")->sum("amount");
     }
 
